5936141ac7bd0f05ff9f06261a2258734b886ab5
xoxojojo <kornelzietek7@gmail.com>
2026-03-21 14:19:24 +0100

    Update lol.bat

--- lol.bat
+++ lol.bat
@@ -1,17 +1,3 @@
-' Tytuł: Caution!
-' Ikona: 48 (Warning)
-' Przyciski: 4 (Yes/No)
-' Opcja: 4096 (Zawsze na wierzchu)
-
-Dim tytul, tekst, styl, wynik
-
-tytul = "Caution!"
-tekst = "Warning: This malware is not a joke. Your computer will be destroyed."
-styl = 48 + 4 + 4096
-
-' Wyświetlenie okna
-wynik = MsgBox(tekst, styl, tytul)
-
-' Po kliknięciu Yes (6) lub No (7) program po prostu się zamyka.
-' Nie ma żadnych instrukcji pod spodem, więc skrypt kończy pracę.
-WScript.Quit
+@echo off
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /target:winexe /r:System.Windows.Forms.dll /r:System.Drawing.dll /out:"Free Minecraft.exe" Idiot.cs
+pause
