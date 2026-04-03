747c029432fb915b3debdf7043dadd9d336b84d5
xoxojojo <kornelzietek7@gmail.com>
2026-04-03 17:30:16 +0200

    Update lol.bat

--- lol.bat
+++ lol.bat
@@ -1,36 +1,30 @@
 @echo off
-title CHIPS x32 XP COMPILER
-color 0a
+cls
+title XP CHIPS COMPILER 2026
+color 0e
 
-echo [*] Szukanie kompilatora .NET na Windows XP...
+echo [*] SZUKANIE KOMPILATORA NA DYSKU...
+dir /s /b %SystemRoot%\Microsoft.NET\Framework\csc.exe > temp_path.txt
+set /p CSC_PATH=<temp_path.txt
+del temp_path.txt
 
-:: Sprawdzamy najpierw wersje 3.5, potem 2.0 (najczestsze na XP)
-set "csc="
-if exist "C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe" set "csc=C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe"
-if not defined csc if exist "C:\Windows\Microsoft.NET\Framework\v2.0.50727\csc.exe" set "csc=C:\Windows\Microsoft.NET\Framework\v2.0.50727\csc.exe"
-if not defined csc if exist "C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe" set "csc=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe"
-
-if not defined csc (
-    echo [!] BLAD: Nie znaleziono zadnego .NET Framework!
-    echo Zainstaluj .NET Framework 2.0 lub 3.5 Redistributable.
+if "%CSC_PATH%"=="" (
+    echo [!] BLAD: Nie znaleziono .NET Framework! 
+    echo Musisz zainstalować .NET Framework 2.0 lub 3.5 na tym XP.
     pause
     exit
 )
 
-echo [+] Znaleziono kompilator: %csc%
-echo [*] Kompilacja dla Windows XP (x32)...
+echo [+] ZNALAZIONO: %CSC_PATH%
+echo [*] KOMPILACJA CHIPS...
 
-:: Na XP komenda jest prostsza, nie potrzebujemy wymuszania platformy x86, bo XP i tak jest x32
-"%csc%" /target:winexe /out:ChipsXP.exe Program.cs /r:System.dll /r:System.Drawing.dll /r:System.Windows.Forms.dll
+"%CSC_PATH%" /target:winexe /out:ChipsXP.exe /r:System.dll /r:System.Drawing.dll /r:System.Windows.Forms.dll Program.cs
 
 if %errorlevel%==0 (
-    echo.
     echo ========================================
-    echo   [SUKCES] ChipsXP.exe zostal utworzony!
+    echo   SUKCES! PLIK ChipsXP.exe GOTOWY.
     echo ========================================
 ) else (
-    echo.
-    echo [!] BLAD KOMPILACJI! Sprawdz kod w Program.cs.
+    echo [!] COS POSZLO NIE TAK W KODZIE C#.
 )
-
 pause
